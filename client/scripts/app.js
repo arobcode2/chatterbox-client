351d95a9f4647da14032daf16015f4edc5c322e2
arobcode2 <andrew-c-rob@live.com>
2018-06-22 16:57:24 -0500

    Implement send method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -10,33 +10,60 @@ class App {
   init() {
   
   }
+
+  send(message) {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+    });
+  }
 };
 
 const app = new App();
 
+var message = {
+  username: 'titan',
+  text: 'trololo',
+  roomname: '4chan'
+};
 
-
-
-
-
-
-
-
-$(document).ready(function(){
-  $.ajax({
-    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      myData = data.results;
-      //$("#chats").append("<div></div>").text(data.results[0].objectId);
-      //console.log(data.results);
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to get message', data);
-    }
-  });
-  //console.log('mydata ' + myData);
+$(document).ready(function() {
+  app.send(message);
 });
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   $.ajax({
+//     url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+//     type: 'GET',
+//     contentType: 'application/json',
+//     success: function (data) {
+//       myData = data.results;
+//       //$("#chats").append("<div></div>").text(data.results[0].objectId);
+//       //console.log(data.results);
+//     },
+//     error: function (data) {
+//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.error('chatterbox: Failed to get message', data);
+//     }
+//   });
+//   //console.log('mydata ' + myData);
+// });
 // 
