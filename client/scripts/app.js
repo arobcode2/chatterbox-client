e75143a848ac22a2b92819c6f5b358b393b8111b
arobcode2 <andrew-c-rob@live.com>
2018-06-22 16:32:48 -0500

    Implement app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,34 @@
 // YOUR CODE HERE:
+//http://parse.atx.hackreactor.com/chatterbox/classes/messages
+
+//let myData = [];
+let app = {};
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $.ajax({
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      myData = data.results;
+      //$("#chats").append("<div></div>").text(data.results[0].objectId);
+      //console.log(data.results);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message', data);
+    }
+  });
+  //console.log('mydata ' + myData);
+});
+// 
